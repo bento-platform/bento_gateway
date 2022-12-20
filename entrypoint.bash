853a7a3ec4995a7534f0d44f89166a0b69783ca2
David Lougheed <david.lougheed@gmail.com>
2022-12-20 13:26:02 -0500

    chore: handle service conf templates

--- entrypoint.bash
+++ entrypoint.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# WORKDIR: /gateway
+
 # Gather a list of all environment variables to use for the NGINX conf. template:
 touch ./VARIABLES
 echo "[bento_gateway] [entrypoint] gathering environment variables for configuration templates"
@@ -10,12 +12,20 @@ for v in $(env | awk -F "=" '{print $1}' | grep "CHORD*"); do
   echo "\${${v}}" >> ./VARIABLES
 done
 
-# Process the NGINX conf. template, using only the selected variables:
+# Process the main NGINX conf. template, using only the selected variables:
 #  - this avoids the ${DOLLAR}-type hack needed before
-echo "[bento_gateway] [entrypoint] writing NGINX configuration"
+echo "[bento_gateway] [entrypoint] writing main NGINX configuration"
 envsubst "$(cat ./VARIABLES)" \
   < ./conf/nginx.conf.tpl \
   > /usr/local/openresty/nginx/conf/nginx.conf
+
+# Process any service templates, using only the selected variables:
+echo "[bento_gateway] [entrypoint] writing service NGINX configuration"
+for f in $(ls /usr/local/openresty/nginx/conf/bento_services/*.conf.tpl); do
+  outfile="${f%%.*}.conf"
+  echo "[bento_gateway] [entrypoint]    writing ${outfile}"
+  envsubst "$(cat ./VARIABLES)" < "${f}" > "${outfile}"
+done
 
 # Start OpenResty
 echo "[bento_gateway] [entrypoint] starting OpenResty"
