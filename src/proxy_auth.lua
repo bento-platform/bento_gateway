7cfa38d7e5beaf6655ef77ef38c8ca021a02c8b8
David Lougheed <david.lougheed@gmail.com>
2023-03-01 15:06:11 -0500

    chore: configurable redis connection string

--- src/proxy_auth.lua
+++ src/proxy_auth.lua
@@ -88,8 +88,11 @@ local ONE_TIME_TOKENS_INVALIDATE_ALL_PATH = "/api/auth/ott/invalidate_all"
 local TEMP_TOKENS_GENERATE_PATH = "/api/auth/tt/generate"
 
 
--- TODO: Make this configurable
-local REDIS_CONNECTION_STRING = "bentov2-redis:6379"
+local REDIS_CONNECTION_STRING = os.getenv("REDIS_CONNECTION_STRING")
+if REDIS_CONNECTION_STRING == nil or REDIS_CONNECTION_STRING == "" then
+  -- Default / backwards-compatibility before this was configurable:
+  REDIS_CONNECTION_STRING = "bentov2-redis:6379"
+end
 
 local REDIS_SOCKET
 local REDIS_HOST
