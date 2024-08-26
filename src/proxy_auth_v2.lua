397139d808d47d7a97b9c254ced885085af4fc3c
David Lougheed <david.lougheed@gmail.com>
2024-08-26 13:57:10 -0400

    chore: allow all Katsu GET projects/datasets endpoints

--- src/proxy_auth_v2.lua
+++ src/proxy_auth_v2.lua
@@ -60,7 +60,8 @@ if (req_method == "GET" or req_method == "OPTIONS") and (
   uri == "/service-info" or  -- any service-info endpoint; rewritten from original /api/.../service-info
   uri == "/workflows" or  -- any workflow-providing endpoint; rewritten from original /api/.../workflows
   uri:sub(1, 11) == "/workflows/" or  -- "
-  req_uri_no_qp == "/api/metadata/api/projects" or
+  req_uri_no_qp:sub(1, 26) == "/api/metadata/api/projects" or
+  req_uri_no_qp:sub(1, 26) == "/api/metadata/api/datasets" or
   req_uri_no_qp == "/api/metadata/api/public" or
   req_uri_no_qp == "/api/metadata/api/public_overview" or
   req_uri_no_qp == "/api/metadata/api/public_search_fields" or
