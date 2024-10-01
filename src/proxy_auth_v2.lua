98b68cdaf40063db36db93e135dc57b04058bf06
David Lougheed <david.lougheed@gmail.com>
2024-10-01 10:30:50 -0400

    chore: rm authz exemption for now-removed katsu endpoint public_dataset replaced by /api/projects

--- src/proxy_auth_v2.lua
+++ src/proxy_auth_v2.lua
@@ -65,7 +65,6 @@ if (req_method == "GET" or req_method == "OPTIONS") and (
   req_uri_no_qp == "/api/metadata/api/public" or
   req_uri_no_qp == "/api/metadata/api/public_overview" or
   req_uri_no_qp == "/api/metadata/api/public_search_fields" or
-  req_uri_no_qp == "/api/metadata/api/public_dataset" or
   req_uri_no_qp == "/api/metadata/api/public_rules" or
   req_uri_no_qp == "/api/metadata/api/extra_properties_schema_types" or
   req_uri_no_qp:sub(1, 26) == "/api/metadata/api/schemas/" or
